f0a47fc298a704d4c7fc58e625d1f39046fd4491
Chalmiller <christopher@transcriptic.com>
2020-02-10 19:26:53 -0800

    Initial server and frontend setup

---
+++ leadmanager/leadmanager/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('frontend.urls')),
+    path('', include('leads.urls'))
+]
